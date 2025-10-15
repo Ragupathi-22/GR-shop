a88d42554fb41da18b897ddb79b16de39443a049
Ragupathi-NTPL <ragupathi@naethra.com>
2025-10-15 12:08:31 +0530

    seo 5

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://gr-shop-2.vercel.app', // Your live domain
+  generateRobotsTxt: true,
+  // Optionally add custom paths or exclude private routes
+}
